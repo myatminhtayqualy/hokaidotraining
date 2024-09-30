04fb893ed0f1a22abee72eb02a7562b4cee4d497
myatminhtayqualy <myatminhtay.qualymm@gmail.com>
2024-09-30 16:20:22 +0630

    first version

---
+++ assets/js/common.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+     const headers = document.querySelectorAll('.accordion-header');
+   
+   
+     headers.forEach(header => {
+         header.addEventListener('click',function(){
+             const content = this.nextElementSibling;
+             const icon = header.querySelector('.headertext');
+   
+             icon.classList.toggle('newicon');
+     
+             content.classList.toggle('show');
+           
+           
+         });  
+   
+       
+     });
+});
+   
+
+
+$(document).ready(function() {
+    
+
+     $(".navbartogellericon").on('click',function(e){
+          $(this).toggleClass('show');
+          $('.menuitems').toggleClass('show');
+          $('.customnavbar').toggleClass('bgchange');
+          $("body").toggleClass("noscroll");
+     });
+
+     $(".nav-link").on('click',function(e){
+        $(".nav-link").removeClass("active");
+
+        $(this).addClass("active");
+     });
+
+    
+          $(window).scroll(function(){
+               const footerMain = $("#footer-main");
+               const scrollPosition = $(window).scrollTop();
+               const windowHeight = $(window).height();
+               const footerMainOffset = footerMain.offset().top;
+
+               if (scrollPosition <= 300) {
+                 $(".pagetop").addClass("d-none");
+               } else {
+                 $(".pagetop").removeClass("d-none");
+               }
+           
+               if ($(window).width() <= 768) {
+                    
+                 if (scrollPosition + windowHeight >= footerMainOffset) {
+                   $(".floatbutton").addClass("pos");
+                 } else {
+                   $(".floatbutton").removeClass("pos");
+                 }
+               } else {
+                 if (scrollPosition + windowHeight >= footerMainOffset) {
+                   $(".floatbutton").addClass("pos");
+                 } else {
+                   $(".floatbutton").removeClass("pos");
+                 }
+               }
+
+               //To Top Click
+               setTimeout(() => {
+                    $(".pagetop").click(() => {
+                    window.scrollTo(0, 0);
+                    });
+               }, 300);
+          });
+
+
+
+});
+
